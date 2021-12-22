cf447d23eec38e50783950cba0158f6f2ab063e4
AncientCrow <91416802+AncientCrow@users.noreply.github.com>
2021-12-22 12:46:36 +0300

    add tips

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from dotenv import dotenv_values
 
 
-config = dotenv_values(".env")
+config = dotenv_values(".env") # if smb want to use this code, add '.env' with your Telegram bot token and api token from hotels.com GL HF
 BOT_TOKEN = config['BOT_TOKEN']
 API_TOKEN = config['API_TOKEN']
